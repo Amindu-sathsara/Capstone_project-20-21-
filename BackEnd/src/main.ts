bd2a9f2b44711f35380913f2c7225546b1452d73
Amindu Sathsara <dapsathsara@std.appsc.sab.ac.lk>
2024-09-16 17:39:11 +0530

    initial authentication before finalzing"

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -28,3 +28,61 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+
+//Public API for web page.........................
+
+/*                 users crud operations
+1.create new user: localhost:3000/users
+2.get all users: localhost:3000/users
+3.get single user: localhost:3000/users/:id
+4.update user: localhost:3000/users/:id
+5.delete user: localhost:3000/users/:id
+*/
+
+//                childProfile crud operations
+
+/*
+1.create new childProfile: localhost:3000/childProfile
+2.get all childProfile: localhost:3000/childProfile
+3.get single childProfile: localhost:3000/childProfile/:id
+4.update childProfile: localhost:3000/childProfile/:id
+5.delete childProfile: localhost:3000/childProfile/:id
+*/
+
+
+
+
+
+
+
+
+
+
+//All the authentications and the protected API  concepts come over here....
+
+
+//    Mobile Applications related details .....
+
+/*  
+1.interface1-click on login navigate to interface2- no need backend stuff
+2.login interface- localhost:3000/auth/login     ---Here How to navigate this to users home page interface
+
+
+
+3.interface 3- change password
+4.interface 4- forgot password
+
+*/
+
+
+
+
+// Doctors view- from  the interface (Home page )
+
+/*
+ 1.interface(5)- (there display all the childProfile)
+                What we need to be done :then it display four icon of feactures
+
+ 2.Interface(6)-a.               
+*/
--- BackEnd/src/users/users.module.ts
+++ BackEnd/src/users/users.module.ts
@@ -4,6 +4,7 @@ import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports:[UsersService]          //When I am start working on user authentication implemmentation there I need to userService inside the auth service that' what I export user Service from here...
 })
 export class UsersModule {}
